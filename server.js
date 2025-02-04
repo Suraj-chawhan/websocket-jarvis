f68dac68ba74ee7f8f07f736e403323c57155d54
Suraj-chawhan <surajchauhan442918@gmail.com>
2025-02-04 20:35:32 +0530

    new

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require("dotenv").config(); // Load environment variables
+// Load environment variables
 const express = require("express");
 const cors = require("cors");
 const { Server } = require("socket.io");
